c217eddaf8fa485f653efae3ea128efcb8eb05bf
emeraldgoose <emeraldgoose@gmail.com>
2024-10-16 11:25:53 +0900

    fix: typo

--- math_.py
+++ math_.py
@@ -3,13 +3,13 @@ from typing import List
 def sum_(a: int, b: int) -> int:
     return a+b
 
-def List_sum(a: List, b: List) -> List:
+def list_sum(a: List, b: List) -> List:
     assert len(a)==len(b), "len(a) != len(b)"
     return [i+j for i, j in zip(a,b)]
 
 def substract(a: int, b: int) -> int:
     return a-b
 
-def List_substract(a: List, b: List) -> List:
+def list_substract(a: List, b: List) -> List:
     assert len(a)==len(b), "len(a) != len(b)"
     return [i-j for i, j in zip(a,b)]
